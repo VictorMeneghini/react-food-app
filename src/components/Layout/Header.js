0afbc141e4d3c868099a401e2a002af0e1dc8607
Victor Meneghini <vhmeneghini@gmail.com>
2021-10-19 15:59:37 -0300

    refactor: run eslint-fix and formate code

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,16 @@
-import mealsImage from "../../assets/meals.jpg";
-import classes from "./Header.module.css";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
-const Header = (props) => {
-	return (
-		<>
-			<header className={classes.header}>
-				<h1>ReactMeals</h1>
-				<button>Cart</button>
-			</header>
-			<div className={classes["main-image"]}>
-				<img alt="A table with a bunch of delicious food" src={mealsImage} />
-			</div>
-		</>
-	);
-};
+const Header = () => (
+	<>
+		<header className={classes.header}>
+			<h1>ReactMeals</h1>
+			<button type="button">Cart</button>
+		</header>
+		<div className={classes['main-image']}>
+			<img alt="A table with a bunch of delicious food" src={mealsImage} />
+		</div>
+	</>
+);
 
 export default Header;
